ed0872d8fcc5fb94cfdca22a4dc7b95e953787e2
Christina Pagan <enterprise@christinas-mbp.home>
2021-04-05 20:41:55 -0400

    added api for google maps to use for event location

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,49 +7,45 @@ var eventList =document.querySelector('#eventList');
 
 //OPEN THE CONSOLE!!!! YOU WILL SEE THE OBJECT I AM PULLING FROM AFTER YOU TYPE IN A KEYWORD AND CLICK THE SEARCH BUTTON
 
-var displayEvents = function(eventData) 
-{
-
-    //there must be a better way to display the first five events someone could attend 
-    //i want to pull up the first 3 events? if the event has multiple dates, how can i just display the event with its mult dates underneath?
-    //could we perhaps do a for-loop?
-
-    //What we need 
-    //1. display the same event only once and display it's different dates 
-    //2. images need to look better
-    //3. merged with css and html 
-    //4. venue location needs to be pulled to google maps 
-    //5. special notices when undefined still appear 
-
-    for(var i = 0; i < 5; i++){//doing just first five events maybe we can randonmize it instead? 
-    var display = document.createElement('span');
-    eventList.appendChild(display);
-    var eventName =  eventData._embedded.events[i].name + '<br>';
-    var eventImage = eventData._embedded.events[i].images[0].url;//images is zero because this is the designated icon image
-    var eventDate = eventData._embedded.events[i].dates.start.localDate + '<br>';
-    var venue = eventData._embedded.events[i]._embedded.venues[0].name +'<br><br>';
-    var notice = eventData._embedded.events[i].pleaseNote + '<br>';
-    display.innerHTML = '<br>' + eventName + '<br> <img src='+ eventImage +' width="200px" height="200px"><br>' + eventDate + venue + notice;
-    eventList.appendChild(display);
-    }
-}
-
-
-
-
 
 var eventLocator = function(userEntry) {
     // can only operate by keyword at the moment, you could change the parameter to city or zipcode see ticketmaster docs 
     var apiUrl = "https://app.ticketmaster.com/discovery/v2/events?keyword=" + userEntry + "&apikey=Ao7jWEWwZIMXSxV8bGEoSfgA3ot0V3sh&locale=*";
 
-    fetch(apiUrl).then(function(response){
-        response.json().then(function(data){
-            console.log('API RESPONSE ---> ', data)
-            displayEvents(data)
+    fetch(apiUrl).then(function(eventResponse){
+            return eventResponse.json();
+
+
+        //eventResponse.json().then(function(data){
+            //console.log('API RESPONSE ---> ', data)
+            //displayEvents(data)
             //console.log(data);
         })
-    })
-}
+        .then(function(eventResponse)
+        {
+            displayEvents(eventResponse);
+
+            console.log(eventResponse);
+            var lat = eventResponse._embedded.events[0]._embedded.venues[0].location.latitude;
+            var lon = eventResponse._embedded.events[0]._embedded.venues[0].location.longitude;
+            console.log(lat);
+            console.log(lon);
+
+            return 'https://www.google.com/maps/embed/v1/view?key=AIzaSyA3_evQJhPJ4tmHpozf_Q1eqxhjLmTdTiE&center='+ lat +','+ lon + '&zoom=18&maptype=satellite';
+        })
+
+        .then(function(mapResponse){
+            return mapResponse.json();
+        })
+
+        .then(function(mapResponse){
+
+            //need something here to see it on the webpage
+        })
+    
+
+    }
+
 
 
 
@@ -67,3 +63,24 @@ var formSubmitHandler = function(ev)
 userForm.addEventListener('submit', formSubmitHandler);
 
 
+
+
+
+
+var displayEvents = function(eventData) 
+{
+
+
+   //doing just first five events maybe we can randonmize it instead? 
+    var display = document.createElement('span');
+    eventList.appendChild(display);
+    var eventName =  eventData._embedded.events[0].name + '<br>';
+    
+    var eventImage = eventData._embedded.events[0].images[0].url;//images is zero because this is the designated icon image
+    var eventDate = eventData._embedded.events[0].dates.start.localDate + '<br>';
+    var venue = eventData._embedded.events[0]._embedded.venues[0].name +'<br><br>';
+    var notice = eventData._embedded.events[0].pleaseNote + '<br>';
+    display.innerHTML = '<br>' + eventName + '<br> <img src='+ eventImage +' width="200px" height="200px"><br>' + eventDate + venue;
+    eventList.appendChild(display);
+//}
+}
