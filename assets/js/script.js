6678ba975faf344d2e076b2f2f85ae3477417951
Christina Pagan <enterprise@christinas-mbp.home>
2021-04-05 21:01:38 -0400

    added variables to html to work with styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,15 @@
 var userForm = document.querySelector('#userForm');
 var userEntry = document.querySelector('#userEntry');
 var submitButton = document.querySelector('#submit-button');
-var userSearchTerm = document.querySelector('#userSearchTerm');
 var eventList =document.querySelector('#eventList');
 
-//OPEN THE CONSOLE!!!! YOU WILL SEE THE OBJECT I AM PULLING FROM AFTER YOU TYPE IN A KEYWORD AND CLICK THE SEARCH BUTTON
-
-
 var eventLocator = function(userEntry) {
-    // can only operate by keyword at the moment, you could change the parameter to city or zipcode see ticketmaster docs 
+    // can only operate by keyword at the moment, we may have to change this to add more parameters the user can search with
     var apiUrl = "https://app.ticketmaster.com/discovery/v2/events?keyword=" + userEntry + "&apikey=Ao7jWEWwZIMXSxV8bGEoSfgA3ot0V3sh&locale=*";
 
     fetch(apiUrl).then(function(eventResponse){
             return eventResponse.json();
 
-
-        //eventResponse.json().then(function(data){
-            //console.log('API RESPONSE ---> ', data)
-            //displayEvents(data)
-            //console.log(data);
         })
         .then(function(eventResponse)
         {
@@ -40,7 +31,7 @@ var eventLocator = function(userEntry) {
 
         .then(function(mapResponse){
 
-            //need something here to see it on the webpage
+           //maybe you can add the function for google maps here
         })
     
 
@@ -71,7 +62,8 @@ var displayEvents = function(eventData)
 {
 
 
-   //doing just first five events maybe we can randonmize it instead? 
+   //display just one event, in the future this could be a random event
+
     var display = document.createElement('span');
     eventList.appendChild(display);
     var eventName =  eventData._embedded.events[0].name + '<br>';
@@ -79,7 +71,6 @@ var displayEvents = function(eventData)
     var eventImage = eventData._embedded.events[0].images[0].url;//images is zero because this is the designated icon image
     var eventDate = eventData._embedded.events[0].dates.start.localDate + '<br>';
     var venue = eventData._embedded.events[0]._embedded.venues[0].name +'<br><br>';
-    var notice = eventData._embedded.events[0].pleaseNote + '<br>';
     display.innerHTML = '<br>' + eventName + '<br> <img src='+ eventImage +' width="200px" height="200px"><br>' + eventDate + venue;
     eventList.appendChild(display);
 //}
